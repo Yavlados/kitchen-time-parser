50348a1647e6e1cecc7f2eead0289e9fed42bdd0
Владислав Житенев <31962780+Yavlados@users.noreply.github.com>
2021-09-27 23:54:34 +0300

    new: updated timestamp logic

--- src/models/public/base/crawler.ts
+++ src/models/public/base/crawler.ts
@@ -1,32 +1,39 @@
-import {format} from 'date-fns'
-import config from '../../../config.json'
-import { formatTimestamp } from '../../../utils/format-timestamp'
+import { format } from "date-fns";
+import config from "../../../config.json";
+import { formatTimestamp } from "../../../utils/format-timestamp";
 
-export interface IStamp{
-    start: string
-    duration: string,
-    action: string
+export interface IStamp {
+  start: string;
+  stepTime: string;
+  action: string;
+  uptime: string;
 }
 
 export const StampActionsEnum = {
-    fetch: 'Fetching data',
-    parse: 'Parsing of data',
-    error:  'Error has been occured'
-}
-
+  fetch: "Fetching data",
+  parse: "Parsing of data",
+  error: "Error has been occured",
+  start: "Server start",
+};
 
 export abstract class Crawler {
-    startDate: Date = null
+  startDate: Date = null;
+  stepDate: Date = null;
 
-    constructor(){
-        this.startDate = new Date()
-    }
+  constructor() {
+    this.startDate = new Date();
+    this.stepDate = this.startDate;
+    this.stamp(StampActionsEnum.start);
+  }
 
-    stamp(action: string): IStamp{
-        return {
-            start : format(this.startDate, config.dateFormat),
-            duration: formatTimestamp( this.startDate, new Date() ),
-            action,
-        }
-    }
+  stamp(action: string): IStamp {
+    const stepTime = formatTimestamp(this.stepDate, new Date());
+    this.stepDate = new Date();
+    return {
+      start: format(this.startDate, config.dateFormat),
+      stepTime,
+      action,
+      uptime: formatTimestamp(this.startDate, new Date()),
+    };
+  }
 }
--- src/utils/format-timestamp.ts
+++ src/utils/format-timestamp.ts
@@ -1,8 +1,8 @@
-export function formatTimestamp( startDate: Date, nowDate: Date):string{
+export function formatTimestamp( startDate: Date, nowDate: Date ):string{
     const miliseconds = nowDate.getTime() - startDate.getTime()
     const seconds = Math.floor(miliseconds/1000)
 
-    const data: any = {seconds, miliseconds}
+    const data: any = {seconds, miliseconds: miliseconds % 1000}
     if(seconds >= 60){
         const minutes = Math.floor(seconds/60)
         data.minutes = minutes
